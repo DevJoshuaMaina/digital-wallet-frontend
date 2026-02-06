a985ea2197f3579c18ee0d965ef104dc9b632b17
Joshua Maina <dev.joshuamaina@gmail.com>
2026-02-06 15:42:59 +0300

    refactor: changed code for emptystate

---
+++ digital-wallet-frontend/src/views/DashboardLayout.vue/Router-View/TransferPage.vue/EmptyState.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="text-center py-12">
+    <div class="text-6xl mb-4">{{ icon }}</div>
+    <h3 class="text-xl font-semibold mb-2">{{ message }}</h3>
+    <BaseButton v-if="actionLabel" @click="$emit('action')">{{ actionLabel }}</BaseButton>
+  </div>
+</template>
+
+<script setup>
+import BaseButton from './base/BaseButton.vue'
+
+defineProps({
+  message: { type: String, required: true },
+  icon: { type: String, default: '📭' },
+  actionLabel: String
+})
+
+defineEmits(['action'])
+</script>
