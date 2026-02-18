c5bcd2d7d448df95301466bc7696175eac01c83a
Joshua Maina <dev.joshuamaina@gmail.com>
2026-02-18 11:05:48 +0300

    fix: fixed wallet symbol on dashboard

--- digital-wallet-frontend/src/views/DashboardLayout.vue/Router-View/DashboardPage.vue/WalletCard.vue
+++ digital-wallet-frontend/src/views/DashboardLayout.vue/Router-View/DashboardPage.vue/WalletCard.vue
@@ -1,7 +1,7 @@
 <template>
   <BaseCard>
     <div class="text-center">
-      <h3 class="text-2xl font-bold text-primary-600">?{{ formattedBalance }}</h3>
+      <h3 class="text-2xl font-bold text-primary-600">₦{{ formattedBalance }}</h3>
       <p class="text-gray-600">Wallet Number: {{ walletNumber }}</p>
       <div class="mt-4">
         <BaseButton @click="$emit('add-money')">Add Money</BaseButton>
